13335a9532e9b62fafd97002e42c6b3b1977273f
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2024-08-30 11:49:26 +0530

    Update TeamMembers.js

--- src/assets/TeamMembers/TeamMembers.js
+++ src/assets/TeamMembers/TeamMembers.js
@@ -2,7 +2,7 @@ import Aakansh from './Aakansh.png'
 import Ayush from './ayush.jpg'
 import meghana from './meghana.png'
 import naman from './naman.jpg'
-import Charishma from './Charishma.png'
+import Charishma from './Charishma.jpg'
 import anuroop from './anuroop.jpg'
 import Koushik from './Koushik.jpg'
 
